68def67231d31483534518ad3a642afe04528c3c
lucasvegad <157348723+lucasvegad@users.noreply.github.com>
2026-02-09 17:45:37 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-};
+  // No pongas 'distDir' ni cosas raras
+  // Dejalo simple así:
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
